2d18880dddf2820c7ebd1d9363c9d8e8eca688d8
mona <md131376st@gmail.com>
2024-02-28 14:05:52 +0100

    last changes

--- indexing/urls.py
+++ indexing/urls.py
@@ -6,8 +6,8 @@ from indexing.view.celeryMangerView import CeleryManger
 
 app_name = 'indexing'
 urlpatterns = [
-    path('taskstate/<uuid:id>/', CeleryManger.as_view()),  # gives the state of the tasks #task/state/<uuid:id>/
-    path('indexretrieval', IndexRetrivalView.as_view()),
+    path('task/state/<uuid:id>/', CeleryManger.as_view()),  # gives the state of the tasks #task/state/<uuid:id>/
+    path('index/retrieve', IndexRetrivalView.as_view()),
     # data una domanda ritorna i documenti più rilevanti # document/retri
-    path('indexcreation', IndexCreationView.as_view())  # crea un indice della code base
+    path('index/store', IndexCreationView.as_view())  # crea un indice della code base
 ]
